3e8df711d15f5ccec39f40320a55122b37b549d1
Ruano12 <mah.benjamim12@gmail.com>
2018-11-11 11:18:11 -0200

    Criando ticket controllet pte 1 - Aula 47

---
+++ HelpDesk/src/main/java/br/com/helpDesk/api/controller/TicketController.java
@@ -0,0 +1,79 @@
+package br.com.helpDesk.api.controller;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.helpDesk.api.response.Response;
+import br.com.helpDesk.api.security.jwt.JwtTokenUtil;
+import br.com.helpDesk.api.service.TicketService;
+import br.com.helpDesk.api.service.UserService;
+import br.com.helpDesk.entity.Ticket;
+import br.com.helpDesk.entity.User;
+import br.com.helpDesk.enums.StatusEnum;
+
+@RestController
+@RequestMapping("/api/ticket")
+@CrossOrigin(origins = "*")
+public class TicketController {
+	
+	@Autowired
+	private TicketService ticketService;
+	
+	@Autowired
+	protected JwtTokenUtil jwtTokenUtil;
+	
+	@Autowired
+	private UserService userService;
+	
+	public ResponseEntity<Response<Ticket>> create(HttpServletRequest request, @RequestBody Ticket ticket, 
+			BindingResult result){
+		Response<Ticket> response = new Response<Ticket>();
+		
+		try {
+			validateCreateTicket(ticket, result);
+			if(result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			ticket.setStatus(StatusEnum.New);
+			ticket.setUser(userFromRequest(request));
+			ticket.setDate(Calendar.getInstance().getTime());
+			ticket.setNumber(generateNumber());
+			Ticket ticketPersist = (Ticket) ticketService.createOrUpdate(ticket);
+			response.setData(ticketPersist);
+		}catch(Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		return ResponseEntity.ok(response);
+	}
+	
+	private void validateCreateTicket(Ticket ticket, BindingResult result) {
+		if(ticket.getTitle() == null) {
+			result.addError(new ObjectError("Ticket", "Titulo não informado"));
+		}
+	}
+	
+	private User userFromRequest(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		String email = jwtTokenUtil.getUsernameFromToken(token);
+		return userService.findByEmail(email);
+	}
+	
+	private Integer generateNumber() {
+		Random random = new Random();
+		return random.nextInt();
+	}
+}
